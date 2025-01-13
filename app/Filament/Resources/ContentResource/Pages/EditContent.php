7bb04381e37107a4ea006536afd4ce0c0e9c169b
Akhil-Techsoft <techsofttest@gmail.com>
2025-01-13 16:39:15 +0530

    Remove delete from admin

--- app/Filament/Resources/ContentResource/Pages/EditContent.php
+++ app/Filament/Resources/ContentResource/Pages/EditContent.php
@@ -13,7 +13,7 @@ class EditContent extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            //Actions\DeleteAction::make(),
         ];
     }
 }
